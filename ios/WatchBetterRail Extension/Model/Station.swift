a69b7767fcbf9eef51db304956cec32165973192
Guy Tepper <guytepper@gmail.com>
2021-05-12 17:34:55 +0300

    Update Station.swift

--- ios/WatchBetterRail Extension/Model/Station.swift
+++ ios/WatchBetterRail Extension/Model/Station.swift
@@ -3,4 +3,7 @@ import Foundation
 struct Station: Hashable, Codable, Identifiable {
     var id: String
     var hebrew: String
+    var arabic: String
+    var english: String
+    var russian: String
 }
