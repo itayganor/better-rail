f06d679512f28950155db7b7f5e7ccd50c54d169
Guy Tepper <guytepper@gmail.com>
2021-05-14 13:30:49 +0300

    Added react-native-watch-connectivity

--- app/models/favorites/favorites.ts
+++ app/models/favorites/favorites.ts
@@ -1,9 +1,19 @@
 import { Instance, SnapshotOut, types } from "mobx-state-tree"
+import { updateApplicationContext, getIsWatchAppInstalled } from "react-native-watch-connectivity"
+
+let isWatchInstalled = false
+
+getIsWatchAppInstalled().then((result) => {
+  if (result === true) {
+    isWatchInstalled = true
+  }
+})
 
 export const favoriteRouteSchema = {
   id: types.string,
   originId: types.string,
   destinationId: types.string,
+  order: types.number,
 }
 
 /**
@@ -16,7 +26,8 @@ export const FavoritesModel = types
   })
   .actions((self) => ({
     add(route: FavoriteRoute) {
-      self.routes.push(route)
+      console.log(self.routes.length)
+      self.routes.push({ ...route, order: self.routes.length || 0 })
     },
     remove(route: FavoriteRoute) {
       const filteredFavorites = self.routes.filter((favorite) => favorite.id !== route.id)
